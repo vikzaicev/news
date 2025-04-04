24f2043433e257fcca21f2d0f87a468f6240a5f2
vikzaicev <vikzaicev@mail.ru>
2025-04-04 06:54:32 +0500

    R-2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,41 @@
 import { formatDate } from "../../helpers/formatDate"
 import style from "./style.module.css"
 
+/*------------Codewars---------------*/
+function findAB(numbers, c) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] * numbers[j] == c) {
+                return [numbers[i], numbers[j]];
+
+            }
+        }
+    }
+    return null;
+}
+//findAB([1, 2, 3, 4, 5, 6], 15)
+
+
+function remove(str, what) {
+    let res = []
+
+    for (let char of str) {
+        for (let key in what) {
+            if (char !== key) {
+                what[key] = +what[key] - 1
+                //console.log(key, what[key]);
+                res.push(char)
+            }
+        }
+    }
+    console.log(res);
+
+}
+//('this is a string', { 't': 1, 'i': 2 })
+
+
+/*------------Codewars---------------*/
+
 export const Header = () => {
     return (
         <header className={style.header}>
---
+++ src/components/Skeleton/Skeleton.jsx
@@ -0,0 +1,20 @@
+import style from "./style.module.css"
+
+
+export const Skeleton = ({ count = 1, type = 'baner' }) => {
+    return (
+        <>
+            {count > 1 ? (
+                <ul className={style.list}>
+                    {[...Array(count)].map((_, index) => (
+
+
+                        <li key={index} className={type === 'baner' ? style.baner : style.item}></li>
+                    ))}
+                </ul>
+            ) : <li className={type === 'item' ? style.item : style.baner}></li>
+
+            }
+        </>
+    )
+}
