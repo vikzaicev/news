69be6557fbbcc5b41472edd536c690b9f52f53ea
vikzaicev <vikzaicev@mail.ru>
2025-04-14 12:37:04 +0500

    3

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,19 +1,14 @@
 import style from "./style.module.css"
 import { NewsBaner } from "../../components/NewsBaner/NewsBaner"
 import { useEffect, useState } from "react"
-<<<<<<< HEAD
-import { getCategories, getNews } from "../../api/api"
-import { NewsList } from "../../components/NewsList/NewsList"
-import { Skeleton } from "../../components/Skeleton/Skeleton"
-import { Pagination } from "../../components/Pagination/Pagination"
-import { Categori } from "../../components/Categori/Categori"
-=======
+
 import { getLangue, getNews } from "../../api/api"
 import { NewsList } from "../../components/NewsList/NewsList"
 import { Skeleton } from "../../components/Skeleton/Skeleton"
 import { Pagination } from "../../components/Pagination/Pagination"
 import { Langue } from "../../components/Langue/Langue"
->>>>>>> develop
+import { Categori } from "../../components/Categori/Categori"
+
 
 
 export const Main = () => {
@@ -100,11 +95,7 @@ export const Main = () => {
 
     return (
         <main className={style.main}>
-<<<<<<< HEAD
             <Categori categories={categories} selectCategory={selectCategory} setSelectCategory={setSelectCategory} />
-=======
-            <Langue languages={languages} />
->>>>>>> develop
             {news.length > 0 && !isLoading ? <NewsBaner item={news[0]} /> : <Skeleton count={1} type={'baner'} />}
             <Pagination
                 handNextPage={handNextPage}
